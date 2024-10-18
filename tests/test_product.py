7d02672166a4b0c7e62c6e13263308a2f5da738f
Алексей Булгаков <Alksbulgakov@gmail.com>
2024-10-18 22:07:14 +0300

    написаны тесты для классав, код соответсвует PEP8, написан README.md

--- tests/test_product.py
+++ tests/test_product.py
@@ -1,20 +1,14 @@
-
-
 def test_product_init_1(test_product_samsung):
     "Функция проверяет инициализацию экземпляра класса Product"
-    assert test_product_samsung.name == 'Samsung Galaxy S23 Ultra'
-    assert test_product_samsung.description == '256GB, Серый цвет, 200MP камера'
-    assert test_product_samsung.price ==  180000.0
+    assert test_product_samsung.name == "Samsung Galaxy S23 Ultra"
+    assert test_product_samsung.description == "256GB, Серый цвет, 200MP камера"
+    assert test_product_samsung.price == 180000.0
     assert test_product_samsung.quantity == 5
+
 
 def test_product_inti_2(test_product_tv):
     "Функция проверяет инициализацию экземпляра класса Product"
     assert test_product_tv.name == '55" QLED 4K'
-    assert test_product_tv.description == 'Фоновая подсветка'
+    assert test_product_tv.description == "Фоновая подсветка"
     assert test_product_tv.price == 123000.0
     assert test_product_tv.quantity == 7
-
-
-
-
-
