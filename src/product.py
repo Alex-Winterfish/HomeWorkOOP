8341e52a9160aad86a6c7ab552892ca8943a9d89
Алексей <alksbulgakov@gmail.com>
2024-10-24 13:32:34 +0300

    В класссах Product и Category созданы приватные атрибуты и методы для их именения, написаны тесты

--- src/product.py
+++ src/product.py
@@ -1,14 +1,9 @@
-
-
-
 class Product:
     """Класс Product принимает описание единицы товар"""
 
     name: str
     description: str
-    price: float
     quantity: int
-    new_product: dict
 
     def __init__(self, name, description, price, quantity):
         self.name = name
@@ -21,28 +16,19 @@ class Product:
         return self.__price
 
     @price.setter
-    def price(self,price):
-
-        if self.__price > price:
-            user_input = input('Новая цена меньше текущей. Подтвердить? y/n: ')
-            if user_input == 'y' and price >= 0:
-                self.__price = price
-            else:
-                self.__price =self.__price
-        if price < 0:
-            self.__price = self.__price
-            print('Цена не должна быть нулевая или отрицательная')
-
-
+    def price(self, price):
+        self.__price = price
 
     @classmethod
-    def new_product(cls,new_product,):
-        name = new_product.get('name')
-        description = new_product.get('description')
-        price = new_product.get('price')
-        quantity = new_product.get('quantity')
-        return cls(name,description,price,quantity)
-
+    def new_product(
+        cls,
+        new_product,
+    ):
+        name = new_product.get("name")
+        description = new_product.get("description")
+        price = new_product.get("price")
+        quantity = new_product.get("quantity")
+        return cls(name, description, price, quantity)
 
 
 class Category:
@@ -50,7 +36,6 @@ class Category:
 
     name: str
     description: str
-    products: list
     category_count = 0
     product_count = 0
 
@@ -59,15 +44,14 @@ class Category:
         self.description = description
         self.__products = products
         Category.category_count += 1
-        Category.product_count += len(products)
+        Category.product_count += len(self.__products)
 
     @property
     def products(self):
+
         for product in self.__products:
-            return f'{product.name}, {product.price} руб. Остаток: {product.quantity}'
+            print(f"{product.name}, {product.price} руб. Остаток: {product.quantity}")
 
-    def add_product(self,product):
+    def add_product(self, product):
+        self.product_count += 1
         return self.__products.append(product)
-
-
-
--- tests/test_product.py
+++ tests/test_product.py
@@ -1,3 +1,6 @@
+from src.product import Product
+
+
 def test_product_init_1(test_product_samsung):
     "Функция проверяет инициализацию экземпляра класса Product"
     assert test_product_samsung.name == "Samsung Galaxy S23 Ultra"
@@ -6,10 +9,21 @@ def test_product_init_1(test_product_samsung):
     assert test_product_samsung.quantity == 5
 
 
-def test_product_inti_2(test_product_tv):
+def test_product_inti_2(test_product_tv_1):
     "Функция проверяет инициализацию экземпляра класса Product"
-    assert test_product_tv.name == '55" QLED 4K'
-    assert test_product_tv.description == "Фоновая подсветка"
-    assert test_product_tv.price == 123000.0
-    assert test_product_tv.quantity == 7
+    assert test_product_tv_1.name == '55" QLED 4K'
+    assert test_product_tv_1.description == "Фоновая подсветка"
+    assert test_product_tv_1.price == 123000.0
+    assert test_product_tv_1.quantity == 7
 
+
+def test_product_change_price(test_product_tv_1, new_price_1):
+    "Функция проверяет изменение цены товара"
+    assert test_product_tv_1.price == 123000.0
+    test_product_tv_1.price = new_price_1
+    assert test_product_tv_1.price == 500
+
+
+def test_new_product(new_product):
+    product = Product.new_product(new_product)
+    assert product.name == "Samsung Galaxy S23 Ultra"
